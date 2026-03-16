cbe8ed5f86b9f6dcc2b5e761fbb0be01cec0dad6
Sergio Alejandro <sergioromero2k@gmail.com>
2026-03-16 08:30:15 +0100

    advance algorithm

--- algorithm/waiting_time_algorithm.py
+++ algorithm/waiting_time_algorithm.py
@@ -1,3 +1,43 @@
 #!/usr/bin/env python3
 
 def get_best_task(candidates, tasks):
+    best_task = 0
+    best_task_time = 0x3f3f3f3f
+    for c in candidates:
+        if tasks[c] < best_task_time:
+            best_task = c
+            best_task_time = tasks[c]
+    return best_task
+
+
+def order_tasks(tasks):
+    candidates = set()
+    for i in range(len(tasks)):
+        candidates.add(i)
+    sol = []
+    while candidates:
+        best = get_best_task(candidates, tasks)
+        candidates.remove(best)
+        sol.append(best)
+    return sol
+
+
+def calculate_waiting_time(sol, tasks):
+    times = []
+    accum = 0
+    for task in sol:
+        accum += tasks[task]
+        times.append(accum)
+    print(times)
+    print(sum(times))
+
+
+def main() -> None:
+    tasks = [5, 10, 3]
+    sol = order_tasks(tasks)
+    print(sol)
+    calculate_waiting_time(sol, tasks)
+
+
+if __name__ == '__main__':
+    main()
--- voraces/7_p42/7_p42.py
+++ voraces/7_p42/7_p42.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python3
 
+def get_best_item(candidates, profit):
+    best_item = -1
+    best_profit = -1
+    for c in candidates:
+        if profit[c] > best_profit:
+            best_profit = profit[c]
+            best_item = c
+        return best_item
+
+def greedy_schedule(profit, deadline):
+    n = len(profit)
+    candidates = set()
+    for i in range(n):
+        candidates.add(i)
+    last_date = max(deadline)
+    sol = [-1] * (last_date + 1)
+    j = 0
+    while candidates and j <= last_date:
+
+
+
+
+def main() -> None:
+    pass
+
+if __name__ == '__main__':
+    main()
